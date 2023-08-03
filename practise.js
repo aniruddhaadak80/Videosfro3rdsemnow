2e3cae62478fa2c307cd460144f4f859151ee076
ANIRUDDHA ADAK <127435065+aniruddhaadak80@users.noreply.github.com>
2023-08-04 02:01:44 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,66 @@
+// firstname='Lata';
+
+
+// function hello(){
+//     return "Hello world!";
+// }
+
+// function a(){
+//     return "Hello a!";
+// }
+
+// function b(){
+//     return 'Hello b!';
+// }
+
+
+
+// function greet(){
+//    return 'Haydo';
+// }
+
+// let salutation=greet();
+
+
+// function echo(input){
+// return input;
+// }
+
+// let x=echo("Greta");
+// let y=echo("CO2");
+
+
+// function greet(name){
+//  return   Hello+"name";
+// }
+
+// let x=greet("Grace");
+// let y=greet("Ada");
+
+
+
+// function toCase(name){
+//     let x=toCase.toLowerCase()+"-"+toCase.toUpperCase();
+//     return x;
+// }
+
+
+
+
+// function reusableFunction(){
+// console.log("Hi World");
+//  }
+
+
+
+// function functionWithArgs(a,b){
+// console.log(a,b);
+// }
+// let x=functionWithArgs(7,9);
+ 
+
+
+function firstChar(str){
+    let trimmer=str.trim();
+     let x=str.charAt(0);
+}
